1f53e304dc7a65be4b546af920df6ad79b18f542
Ula Buczak <mankowskau@gmail.com>
2021-07-07 23:27:05 +0200

    Add styling to new item

--- src/js/app.js
+++ src/js/app.js
@@ -11,25 +11,39 @@ const app = {
     this.toDoButton.addEventListener('click', e => {
       e.preventDefault();
 
-      const todoDiv = document.createElement('div');
-      todoDiv.classList.add('.todo');
-      const newToDo = document.createElement('li');
-      newToDo.innerText = 'hej';
-      newToDo.classList.add('toDo__newItem');
-      todoDiv.appendChild(newToDo);
+      this.createToDoDiv();
+      this.createCompletedBtn();
+      this.createTrashBtn();
 
-      const completedButton = document.createElement('button');
-      completedButton.innerHTML = '<i class="fas fa-check"></i>';
-      completedButton.classList.add('completed-btn');
-      todoDiv.appendChild(completedButton);
-
-      const trashButton = document.createElement('button');
-      trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-      trashButton.classList.add('completed-btn');
-      todoDiv.appendChild(trashButton);
-
-      this.toDoList.appendChild(todoDiv);
+      this.toDoList.appendChild(this.todoDiv);
+      this.toDoInput.value = '';
     });
+  },
+
+  createToDoDiv: function () {
+
+    this.todoDiv = document.createElement('div');
+    this.todoDiv.classList.add('todo');
+    const newToDo = document.createElement('li');
+    newToDo.innerText = this.toDoInput.value;
+    newToDo.classList.add('toDo__newItem');
+    this.todoDiv.appendChild(newToDo);
+  },
+
+  createCompletedBtn: function () {
+
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('completed-btn');
+    this.todoDiv.appendChild(completedButton);
+  },
+
+  createTrashBtn: function () {
+
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    this.todoDiv.appendChild(trashButton);
   },
 
   init: function () {
